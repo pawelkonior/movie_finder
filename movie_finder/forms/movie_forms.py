63c42da898c380715dbc68eeb3a4c898e8fd1996
Pawel Konior <pawel.konior@gmail.com>
2021-06-15 22:15:06 +0200

    bootstrap, layout, components, forms

--- movie_finder/forms/movie_forms.py
+++ movie_finder/forms/movie_forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, TextAreaField, FloatField, FileField, SubmitField
+from wtforms.validators import DataRequired
 
 
 class AddMovieForm(FlaskForm):
-    title = StringField("Title")
+    title = StringField("Title", validators=[DataRequired("Title is required, hommie!")])
     description = TextAreaField("Description")
     duration = FloatField("Duration")
     image = FileField("Cover")
